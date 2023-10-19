9d1d1ae89f0479867624daa5d7609b5ab122eda4
Erickkaranja <karanja99erick@gmail.com>
2023-10-19 23:32:50 +0300

    stat count

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+# Define the regular expression pattern to match the input format
+pattern = r'^(\S+) - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
+
+# Initialize variables to store metrics
+total_size = 0
+status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
+                 405: 0, 500: 0}
+line_count = 0
+
+try:
+    for line in sys.stdin:
+        # Match the line against the pattern
+        match = re.match(pattern, line)
+        if match:
+            # Extract status code and file size
+            status_code = int(match.group(2))
+            file_size = int(match.group(3))
+
+            # Update total file size and status code counts
+            total_size += file_size
+            if status_code in status_counts:
+                status_counts[status_code] += 1
+
+            line_count += 1
+
+        # Print statistics after every 10 lines
+        if line_count % 10 == 0:
+            print(f'Total file size: {total_size}')
+            for code, count in sorted(status_counts.items()):
+                if count > 0:
+                    print(f'{code}: {count}')
+
+except KeyboardInterrupt:
+    # Handle Ctrl+C by printing the statistics
+    pass
