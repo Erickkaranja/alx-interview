0532234c424979c95ab873a9fd7bd182797f280c
Erickkaranja <karanja99erick@gmail.com>
2024-01-31 14:47:57 +0300

    removing print statement

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -13,12 +13,3 @@ def canUnlockAll(boxes):
     if len(check_list) == 0:
         return True
     return False
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
