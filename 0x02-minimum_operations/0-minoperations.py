8f9d4c9ce05471e3db2101f00f2f39c630c983b7
Erickkaranja <karanja99erick@gmail.com>
2024-01-31 22:29:35 +0300

    updating task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,13 +11,17 @@ def minOperations(n):
     '''
     if n <= 1:
         return 0
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(1, int(i**0.5) + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-                if i // j != j:
-                    dp[i] = min(dp[i], dp[i // j] + j)
-    return dp[n]
+    i = 2
+    prime_factors = []
+    while i * i <= n:
+        if n % i == 0:
+            prime_factors.append(i)
+            n //= i
+        else:
+            i += 1
+    if n > 1:
+        prime_factors.append(n)
+    if prime_factors == [1, n]:
+        return (sum(prime_factors) - 1)
+    else:
+        return sum(prime_factors)
