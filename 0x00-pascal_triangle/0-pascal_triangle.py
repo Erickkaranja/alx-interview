8f023bbdb4d11ee53a9f82e4c215a7ceb9e4e439
Erickkaranja <karanja99erick@gmail.com>
2024-01-31 08:59:26 +0300

    solved the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """developing the pascal triagle"""
+
 
 def pascal_triangle(n):
     '''creates pascal's triangle'''
     if n <= 0:
         return []
+    result = []
     for i in range(0, n):
-        val = 1
-        for j in range(0, i + 1):
-            print(val, end=" ")
-            val = val * (i - j) // (j + 1)
-        print()
+        x = str(11 ** i)
+        result.append(x)
+    return result
